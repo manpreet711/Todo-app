29e5643fa8e85315c8e2f2f099ad89cc41117d0a
Manpreet singh <69038609+manpreet711@users.noreply.github.com>
2020-07-31 17:38:32 +0530

    Delete localstorage.js

--- localstorage.js
+++
@@ -1,7 +0,0 @@
-localStorage.setItem('name','manpreet');
-console.log(localStorage.getItem('name'));
-
-localStorage.setItem('manpreet','name');
-console.log(localstorge.getItem('manpreet'));
-
-localStorage.clear()
