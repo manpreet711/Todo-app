258c16e054ee1e8c8d1a4fd7397436e772f950ab
Manpreet singh <manpreetsaini625@gmail.com>
2020-08-04 20:50:03 +0530

    local storage

--- todo.js
+++ todo.js
@@ -2,6 +2,11 @@
 var ul = document.getElementById('list')
 var li;
 // console.log(ul);
+var count = 0
+// let count = Window.localStorage.setItem("count","0");
+if(!count){
+    window.localStorage.setItem("count","0");
+}
 
 var addButon = document.getElementById('add')
 addButon.addEventListener('click', addItem)
@@ -14,43 +19,45 @@ removeallbutton.addEventListener('click', removeallitems)
 
 
 function addItem( ) {
-var input = document.getElementById('input');
-var item = input.value;
+    var input = document.getElementById('input');
+    var item = input.value;
 
-// ul = document.getElementById('list');
-var textnode = document.createTextNode(item)
+    // ul = document.getElementById('list');
+    var textnode = document.createTextNode(item)
 
-if (item === ' ') {
-    // var tag = document.createElement('p');
-    // tag = document.createTextNode('Enter a Todo')
-    // var element =document.getElementById('ptag')
-    // element.appendChild(tag)
-    // console.log('working'); 
-    }else{
-        ///create li
-        li=document.createElement('li')
-        //create checkbox
-        var checkbox = document.createElement('input')
-        checkbox.type = 'checkbox';
-        checkbox.setAttribute('id','check');
-        //create label
-        var label = document.createElement('label')
-        //add these elememts on to web page
-       
-        ul.appendChild(label);
-        li.appendChild(checkbox);
-        label.appendChild(textnode);
-        li.appendChild(label);
+        if (item === '') {
+        // var tag = document.createElement('p');
+        // tag = document.createTextNode('Enter a Todo')
+        // var element =document.getElementById('ptag')
+        // element.appendChild(tag)
+        // console.log('working'); 
+        }else{
+            ///create li
+            li=document.createElement('li')
+            //create checkbox
+            var checkbox = document.createElement('input')
+            checkbox.type = 'checkbox';
+            checkbox.setAttribute('id','check');
+            //create label
+            var label = document.createElement('label')
+            //add these elememts on to web page
+        
+            ul.appendChild(label);
+            li.appendChild(checkbox);
+            label.appendChild(textnode);
+            li.appendChild(label);
 
-        ul.insertBefore(li,ul.childNodes[0]);
-        setTimeout(()=>{
-            li.classname = 'visual';
-        },2);
+            ul.insertBefore(li,ul.childNodes[0]);
+            setTimeout(()=>{
+                li.classname = 'visual';
+            },2);
 
-        input.value = '';
-
-    
-    }
+            input.value = '';
+            window.localStorage.setItem(item,item);
+            count += 1;
+            window.localStorage.setItem("count",count);
+            
+        }  
 }
 
     
